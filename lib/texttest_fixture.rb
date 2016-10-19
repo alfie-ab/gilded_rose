f40a6c0152b1a6696f6b0f420908f575fe3f0c8e
Alfie <alfie_ab@hotmail.co.uk>
2016-10-19 12:27:56 +0100

    tests added to structure refactoring

--- lib/texttest_fixture.rb
+++ lib/texttest_fixture.rb
@@ -1,5 +1,4 @@
 require_relative 'gilded_rose'
-require_relative 'item'
 
 puts "OMGHAI!"
 items = [
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,12 +2,59 @@ require 'gilded_rose'
 
 describe GildedRose do
 
+  QUALITY = 8
+  SELL_IN = 4
+
   describe "#update_quality" do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it "does not change the quality of the Sulfuras item" do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", SELL_IN, QUALITY)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq QUALITY
+      expect(items[0].sell_in).to eq SELL_IN
+    end
+
+    it "ensures that aged brie increases in quality over time" do
+      items = [Item.new("Aged Brie", SELL_IN, QUALITY)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq QUALITY + 1
+      expect(items[0].sell_in).to eq SELL_IN - 1
+    end
+
+    it "ensures that backstage passes increase by 3 with under 5 days left" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", SELL_IN, QUALITY)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq QUALITY + 3
+      expect(items[0].sell_in).to eq SELL_IN - 1
+    end
+
+    it "ensures that backstage passes increase by 2 with over 5 days and under 10 days left" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", (SELL_IN + 2), QUALITY)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq QUALITY + 2
+      expect(items[0].sell_in).to eq SELL_IN + 1
+    end
+
+    it "ensures that quality of item is never negative" do
+      items = [Item.new("banana", SELL_IN, (QUALITY - 8))]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+      expect(items[0].sell_in).to eq SELL_IN - 1
+    end
+
+    it "ensures that quality of item degrades twice as fast after sell by date" do
+      items = [Item.new("banana", (SELL_IN - 4), QUALITY)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 6
+      expect(items[0].sell_in).to eq SELL_IN - 5
+    end
+
+
   end
 
 end
